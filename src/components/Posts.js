eb45474e20b6d3f44cdf695bdb0ecef99c2b471f
harshvardhan jha <harshjha038@gmail.com>
2021-08-24 12:57:04 +0530

    latest changes

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import sanityClient from "../client.js";
 import BlockContent from "@sanity/block-content-to-react";
